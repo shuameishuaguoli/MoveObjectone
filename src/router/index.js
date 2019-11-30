598395cb7b935c13223f4e0c627de331ba0417d4
shuameishuaguoli <shuameishuaguoli@163.com>
2019-11-30 19:11:11 +0800

    3.配置登录页的路由表

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import login from '@/views/LoginPage'
 Vue.use(VueRouter)
 // 配置路由表
 const routes = [
-
+  // 挂载登录页的路由
+  {
+    path: '/login',
+    component: login
+  }
 ]
 
 const router = new VueRouter({
--- src/views/LoginPage/index.vue
+++ src/views/LoginPage/index.vue
@@ -1,7 +1,5 @@
 <template>
-  <div>
-    <router-view></router-view>
-  </div>
+  <div>我是登录登页面</div>
 </template>
 
 <script>
